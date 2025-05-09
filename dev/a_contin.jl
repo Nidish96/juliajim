9b0ca6f01f4f31e5e2f67659292bc74c4771f7f4
Nidish <nidish@iitm.ac.in>
2025-05-09 15:32:46 +0530

    started continuation development

--- dev/a_contin.jl
+++ dev/a_contin.jl
@@ -1,12 +1,12 @@
 # * Preamble
 using NonlinearSolve
-using Accessors
 
 # * Duffing
-function duffresfun!(du, J, u, p, JOm=nothing)
-    Om, z0, w0, al, F = p;
-    A0 = u[1];
-    b0 = u[2];
+function duffresfun!(uOm, p; du=nothing, J=nothing, JOm=nothing)
+    (; z0, w0, al, F) = p;
+    A0 = uOm[1];
+    b0 = uOm[2];
+    Om = uOm[3];
 
     if du !== nothing
         du[:] = [-z0*w0*A0-F/2w0*sin(b0), -(Om-w0)+3al/8w0*A0^2-F/2w0/A0*cos(b0)];
@@ -20,13 +20,60 @@ function duffresfun!(du, J, u, p, JOm=nothing)
     return nothing;
 end
 
-funduff = NonlinearFunction((du,u,p)->duffresfun!(du,nothing,u,p), jac=(J,u,p)->duffresfun!(nothing,J,u,p));
-pars = (Om = 0.1, z0 = 0.1e-2, w0 = 2., al = 0.01, F = 0.1);
+pars = (z0 = 0.1e-2, w0 = 2., al = 0.01, F = 0.1);
+Om = 0.1;
+funduff = NonlinearFunction((du,u,Om)->duffresfun!([u;Om],pars;du=du),
+                            jac=(J,u,Om)->duffresfun!([u;Om],pars;J=J));
 
-Ab0 = [0.01, 0.];
-dprob = NonlinearProblem(funduff, Ab0, collect(pars));
+Ab0 = [1., 0.];
+dprob = NonlinearProblem(funduff, Ab0, Om);
 
 sol = solve(dprob, show_trace=Val(true));
 
-# * Setting using Accessors
-@set (@optic _.Om)(pars)=0.2
+# * Iterator Interface
+nlcache = init(dprob; show_trace=Val(true));
+
+# step!(nlcache);
+
+# * My Attempt
+# ** Define a struct with constructor
+struct myNLSoln
+    u::Union{Nothing, Vector{Float64}}
+    J::Union{Nothing, Matrix{Float64}}
+    Jp::Union{Nothing, Vector{Float64}}
+    dudp::Union{Nothing, Vector{Float64}}
+end
+function myNLSoln(u=nothing; J=nothing, Jp=nothing)
+    if J === nothing || Jp === nothing
+        dudp = nothing;
+    else
+        dudp = -J\Jp;
+    end
+    return myNLSoln(u, J, Jp, dudp);
+end
+function Base.show(io::IO, p::myNLSoln)
+    print(io, " u = $(p.u)\n dudp = $(p.dudp)\n J = $(p.J)\n Jp = $(p.Jp)")
+end
+function Base.:-(v1::myNLSoln, v2::myNLSoln)
+    return myNLSoln(v1.u-v2.u, v1.J-v2.J, v1.Jp-v2.Jp, v1.dudp-v2.dudp);
+end
+
+# ** Try out continuation
+
+# Temporary Variables
+J = zeros(2,2); JOm = zeros(2);
+
+# First 2 Omega values
+Om0 = 0.1; Om1 = 0.15;
+
+# Compute first two points
+prob = remake(dprob; p=Om0);
+sol0 = solve(prob);
+duffresfun!([sol0.u;Om0], pars;J=J,JOm=JOm);
+sol0 = myNLSoln([sol0.u;Om0]; J=copy(J), Jp=copy(JOm));
+prob = remake(prob; u0=sol0.u[1:2], p=Om1);
+sol1 = solve(prob);
+duffresfun!([sol1.u;Om1], pars;J=J, JOm=JOm);
+sol1 = myNLSoln([sol1.u;Om1]; J=copy(J), Jp=copy(JOm));
+
+dsol1 = sol1-sol0;
