3e31d1ceaa7d6e77617b366d608bdee4cda44c65
Nidish <nidish@iitm.ac.in>
2025-08-18 20:16:08 +0530

    Tracking example markdown files.

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using Literate
 using juliajim, juliajim.HARMONIC, juliajim.CONTINUATION, juliajim.MDOFUTILS
 
 # * Generate Example file documentation with Literate.jl
-Literate.markdown("../examples/a_hworld.jl", "./src", documenter=true)
+# Literate.markdown("../examples/a_hworld.jl", "./src", documenter=true)
 
 # * Make the actual doc files
 DocMeta.setdocmeta!(
